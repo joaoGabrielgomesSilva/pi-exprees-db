db2ea6a07102b026fd9daf27665691bcc2fb75ed
joaoGabrielgomesSilva <gabrielgomessilvaj@gmail.com>
2024-07-03 17:37:06 -0300

    atualização 17:36 dia 03/07

--- routes/alunos.js
+++ routes/alunos.js
@@ -9,6 +9,10 @@ router.get("/", function (req, res, next) {
     };
     res.render("list", data);
 });
+router.get('/new',function(req,res,next){
+    res.render('form',{title:"Novo aluno", buttontext:"Adicionar"});
+
+});
 router.get('/:matricula', function(req, res, next) {
     
     const{matricula} = req.params;
@@ -18,4 +22,13 @@ router.get('/:matricula', function(req, res, next) {
     
     res.render('card' , {title:'Detalhe dos alunos',aluno});
 });
+router.get('/edit/:matricula', function(req, res, next) {
+    const{matricula} = req.params;
+
+    const aluno =alunos.content[matricula] ;
+
+    res.render('form',{title:"Editar aluno", buttontext:"Salvar Alterações",aluno});
+});
+
+
 module.exports = router;
