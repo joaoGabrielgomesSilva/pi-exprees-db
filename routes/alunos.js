06ef251de55fe56cab3f03f20848ba40508dda08
joaoGabrielgomesSilva <gabrielgomessilvaj@gmail.com>
2024-07-05 16:39:32 -0300

    atualizçao dia 05/07

--- routes/alunos.js
+++ routes/alunos.js
@@ -1,45 +1,54 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const alunos = require("../test/mocks/alunos.json");
 
 router.get("/", function (req, res, next) {
     const data = {
         title: "Alunos",
-        alunos: alunos
+        alunos: alunos,
     };
     res.render("list", data);
 });
-router.get('/new',function(req,res,next){
-    res.render('form',{title:"Novo aluno", buttontext:"Adicionar"});
-
+router.get("/new", function (req, res, next) {
+    res.render("form", { title: "Novo aluno", buttontext: "Adicionar" });
 });
-router.get('/:matricula', function(req, res, next) {
-    
-    const{matricula} = req.params;
+router.get("/:matricula", function (req, res, next) {
+    const { matricula } = req.params;
 
-    const aluno =alunos.content[matricula] ;
-    
-    
-    res.render('card' , {title:'Detalhe dos alunos',aluno});
+    const aluno = alunos.content[matricula];
+
+    res.render("card", { title: "Detalhe dos alunos", aluno });
 });
-router.get('/edit/:matricula', function(req, res, next) {
-    const{matricula} = req.params;
+router.get("/edit/:matricula", function (req, res, next) {
+    const { matricula } = req.params;
 
-    const aluno =alunos.content[matricula] ;
+    const aluno = alunos.content[matricula];
 
-    res.render('form',{title:"Editar aluno", buttontext:"Salvar Alterações",aluno});
+    res.render("form", {
+        title: "Editar aluno",
+        buttontext: "Salvar Alterações",
+        aluno,
+    });
 });
 
-router.post('/create',function(req,res,next){
-    let novoAluno=req.body;
-    let matricula=novoAluno.matricula
+router.post("/create", function (req, res, next) {
+    let novoAluno = req.body;
+    let matricula = novoAluno.matricula;
 
-    alunos.content[matricula]= {
+    alunos.content[matricula] = {
         ...novoAluno,
-        matricula:Number(matricula),
-        extra:'e'
+        matricula: Number(matricula),
+        extra: "e",
     };
-    res.redirect('/alunos')
-})
+    res.redirect("/alunos");
+});
+
+router.put("/", function (req, res, next) {
+    res.send(rq.body);
+});
+
+router.delete("/", function (req, res, next) {
+    res.send(rq.body);
+});
 
 module.exports = router;
