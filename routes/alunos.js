58ef4f76a25febbb82177d2dca8d1ee2fb9dff7b
joaoGabrielgomesSilva <gabrielgomessilvaj@gmail.com>
2024-07-10 17:28:02 -0300

    atualizção dop dia 10/07

--- routes/alunos.js
+++ routes/alunos.js
@@ -1,82 +1,63 @@
-var express = require("express");
-var router = express.Router();
-const alunos = require("../test/mocks/alunos.json");
-const { json } = require("body-parser");
-const {localApi}= require('../config/config_axios');
-router.get("/", async function (req, res, next) {
-    // const data = {
-    //     title: "Alunos",
-    //     alunos: alunos,
-    // };
-    // res.render("list", data);
+let express = require('express');
+const { localApi } = require('../config/config_axios')
+let router = express.Router();
+let alunos = require('../test/mocks/alunos.json');
+// const { route } = require('.');
+router.get('/', async function (req,res, next){
     try {
-        const {data:alunos }= await localApi.get('/api/v1/alunos')
-        const data = {title:'Alunos' , alunos}
-        res.status(200).render('list' , data)
-
+        const {data:alunos} = await localApi.get('/api/v1/alunos')
+        const data = {title: "Alunos",alunos}
+        res.status(200).render('list',data)
     } catch (error) {
-        res.json({msg: error.massage});
+        res.status(400).json({msg: error.messagem})
     }
+    // const Date = {title: "Alunos",alunos}
+    // res.render('list',Date)
+})
+router.get('/new', function(req,res,next){
+    const parametro = "create";
+    res.render('form',{method: "POST",parametro, title: 'Novo Aluno', buttonText:'Adicionar'})
+})
+router.post('/', function (req, res, next) {
+    const {body,method} = req
+    res.send({body,method})
 });
-router.get("/new", function (req, res, next) {
-  const{heads: labels} = alunos
-  const parametro= "create"
-  const data = {metodo: "POST" , parametro:"create" , titulo:'novo aluno' , buttonText: 'Salvar Aluno' }
-    res.render("form",data);
-});
-router.get("/:matricula", function (req, res, next) {
-    const { matricula:matricula } = 
-
-    res.render("card", { title: "Detalhe dos alunos", aluno });
-});
-router.get("/edit/:matricula", function (req, res, next) {
-    const { matricula } = req.params;
-    const aluno = alunos.content[matricula];
-
-    const data={
-        title: "Editar aluno",
-        buttontext: "Salvar Alterações",
-        aluno,
-        parametro:matricula,
-        method: "PUT"
+router.post('/create', function(req,res,next){
+    const NovoAluno = req.body;
+    const matricula = NovoAluno.matricula
+    alunos.content[matricula] = {
+        ...NovoAluno,
+        matricula: Number(matricula)
     }
-    res.render('form',data)
-});
-
-router.post("/create", function (req, res, next) {
-    let novoAluno = req.body;
-    let matricula = novoAluno.matricula;
-
+    res.redirect('/alunos');
+})
+router.get('/:matricula',function (req,res,next){
+    const {matricula} = req.params
+    const aluno = alunos.content[matricula]
+    res.render('card',{title: 'Detalhes do aluno',aluno});
+})
+router.get('/edit/:matricula',function(req,res,next){
+    const {matricula} = req.params
+    const parametro = matricula
+    const aluno = alunos.content[matricula]
+    res.render('form',{method: "PUT",parametro, title: 'Editar Aluno', buttonText:'Salvar, alterações',aluno})
+})
+router.put('/:matricula', function (req, res, next) {
+    // const {body,method} = req
+    const NovoAluno = req.body
+    const matricula = NovoAluno.matricula
     alunos.content[matricula] = {
-        ...novoAluno,
-        matricula: Number(matricula),
-        extra: "e",
-    };
-    res.redirect("/alunos");
+        ...NovoAluno,
+        matricula: Number(matricula)
+    }
+    // res.send({body,parametro, method,masg: "Alterar aluno"})
+    res.redirect('/alunos')
 });
-
-router.put("/:matricula", function (req, res, next) {
-    // const { body, method } = req;
-    const { matricula } = req.params;
-    let novoAluno = req.body;
-    const aluno = alunos.content[matricula];
-
-    alunos.content[matricula] = {
-        ...novoAluno,
-        matricula: Number(matricula),
-        extra: "e",
-    };
-    res.redirect("/alunos");
-
-    // res.send({ body, method ,msg:'altera usuario'  });
-});
-
-router.delete("/:matricula", function (req, res, next) {
-    const { matricula } = req.params;
+router.delete('/:matricula', function (req, res, next) {
+    // const {body,method} = req
+    const matricula =req.params.matricula;
     delete alunos.content[matricula]
-    
-    res.redirect( 303,'/alunos');
+    res.redirect(303,'/alunos')
+    // res.send({body,method,msg: "Remover aluno"})
 });
-
-
 module.exports = router;
--- routes/api/apialunos.js
+++ routes/api/apialunos.js
@@ -1,67 +1,57 @@
 var express = require("express");
 var router = express.Router();
-let alunos = require("../../test/mocks/alunos.json");
-const { response } = require("../../app");
+// const { response } = require("../../app");
+const db = require("../../config/config_database");
 
-
-router.get("/", function (req, res, next) {
-    // const data = {
-    //     alunos: alunos,
-    // };
-    // res.json( data);
+router.get("/", async function (req, res, next) {
+    const query = "select * from alunos";
     try {
-        res.status(200) .json(alunos);
-        
+        const data = await db.any(query);
+        res.status(200).json(data);
     } catch (error) {
-        res.status(400).json({msg:error.massage});
+        res.status(400).json({ msg: error.massage });
     }
 });
 
-router.get("/:matricula", function (req, res, next) {
-    const {matricula} = req.params
+router.get("/:matricula", async function (req, res, next) {
+    const { matricula } = req.params;
+    const query = "select * from alunos where matricula = $1";
+
     try {
-    const alunos = alunos.content[matricula]
-    res.status(200).json(alunos)
-        
+        const data = await db.one("select * from alunos where matricula = $1", [
+            matricula,
+        ]);
+        res.status(200).json(alunos);
     } catch (error) {
-        res.status(400).json({msg:error.massage});
+        res.status(400).json({ msg: error.massage });
     }
 });
 
+router.post("/", async function (req, res, next) {
+    const matricula = req.body.matricula;
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const data_nascimento = req.body.data_nascimento;
 
-router.post("/", function (req, res, next) {
-const novoAluno=req.body
-const matricula=  Number(req.matricula.matricula)
+    const query = ` insert  into alunos(nome,data_nascimento,email,matricula) values ($1,$2,$3,$4)`;
 
-alunos.content[matricula]={...novoAluno, matricula}
-const response= {
-    msg:"aluno criado",
-    aluno:alunos.content[matricula]
-}
-res.jason(200).json(response)
-
+    const values = [nome, data_nascimento, email, matricula];
+    try {
+        const data = await db.any(query, values);
+        res.status(201).json(data);
+    } catch (error) {
+        res.status(400).json({ msg: error.massage });
+    }
 });
 
 router.put("/:matricula", function (req, res, next) {
-    // const { body, method } = req;
-    // const { matricula } = req.params;
-    // let novoAluno = req.body;
-    // const aluno = alunos.content[matricula];
-
-    // alunos.content[matricula] = {
-    //     ...novoAluno,
-    //     matricula: Number(matricula),
-    //     extra: "e",
-    // };
-    // res.redirect("/alunos");
-
-    const {matricula} = req.params
+    const { matricula } = req.params;
+    const query = `update alunos set nome=$2,data_nacimento=$3,email =$4  where matricula = $1`;
     try {
-    const alunos = alunos.content[matricula]
-    res.status(200).json(alunos)
-        
+        const alunos = alunos.content[matricula];
+        res.status(200).json(alunos);
     } catch (error) {
-        res.status(400).json({msg:error.massage});
+        res.status(400).json({ msg: error.massage });
     }
 
     // res.send({ body, method ,msg:'altera usuario'  });
@@ -69,10 +59,10 @@ router.put("/:matricula", function (req, res, next) {
 
 router.delete("/:matricula", function (req, res, next) {
     const { matricula } = req.params;
-    delete alunos.content[matricula]
-    
-    res.redirect( 303,'/alunos');
-});
+    const query = `delete from   alunos  where matricula = $1`;
+    delete alunos.content[matricula];
 
+    res.redirect(303, "/alunos");
+});
 
 module.exports = router;
